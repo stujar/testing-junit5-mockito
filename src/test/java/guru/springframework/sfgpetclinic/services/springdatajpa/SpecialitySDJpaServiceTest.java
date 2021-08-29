ad2838222bb4f4c2cffed63e8008d14045ac1996
jalnor <norrisj0612@gmail.com>
2021-08-29 00:28:47 -0400

    Created a mock repository and an injected service to test some functionality of the repository.

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class SpecialitySDJpaServiceTest {
+
+    @Mock
+    SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    SpecialitySDJpaService specialitySDJpaService;
+
+    @Test
+    void deleteById() {
+        specialitySDJpaService.deleteById(1L);
+    }
+
+    @Test
+    void testDelete() {
+        specialitySDJpaService.delete(new Speciality());
+
+    }
+}
