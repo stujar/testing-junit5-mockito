67e86f6662737e327b6685755a35d99a6f98b0dd
jalnor <norrisj0612@gmail.com>
2021-08-30 14:56:28 -0400

    Assignment to add bdd tests to the VisitSDJpaServiceTest.

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
@@ -16,6 +16,8 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -45,6 +47,21 @@ class VisitSDJpaServiceTest {
         verify(repo).findAll();
     }
 
+    @DisplayName("BDD Test FindAll")
+    @Test
+    void findAllBDD() {
+        // given
+        given(repo.findAll()).willReturn(Set.of(new Visit(), new Visit(), new Visit()));
+
+        // when
+        Set<Visit> foundVisits = service.findAll();
+
+        // then
+        assertThat(foundVisits).isNotNull();
+        assertThat(foundVisits).hasSize(3);
+        then(repo).should().findAll();
+    }
+
     @DisplayName("Test FindByID")
     @Test
     void findById() {
@@ -52,6 +69,20 @@ class VisitSDJpaServiceTest {
         Visit foundVisit = service.findById(1L);
         assertThat(foundVisit).isNotNull();
         verify(repo).findById(anyLong());
+    }
+
+    @DisplayName("BDD Test FindByID")
+    @Test
+    void findByIdBDD() {
+        // given
+        given(repo.findById(anyLong())).willReturn(Optional.of(visit));
+
+        // when
+        Visit foundVisit = service.findById(1L);
+
+        // then
+        assertThat(foundVisit).isNotNull();
+        then(repo).should().findById(anyLong());
     }
 
     @DisplayName("Test SaveByObject")
@@ -63,6 +94,20 @@ class VisitSDJpaServiceTest {
         assertThat(savedVisit).isEqualTo(visit);
     }
 
+    @DisplayName("BDD Test SaveByObject")
+    @Test
+    void saveBDD() {
+        // given
+        given(repo.save(any(Visit.class))).willReturn(visit);
+
+        // when
+        Visit savedVisit = service.save(visit);
+
+        // then
+        then(repo).should().save(any(Visit.class));
+        assertThat(savedVisit).isEqualTo(visit);
+    }
+
     @DisplayName("Test DeleteByObject")
     @Test
     void delete() {
@@ -70,10 +115,30 @@ class VisitSDJpaServiceTest {
         verify(repo).delete(visit);
     }
 
+    @DisplayName("BDD Test DeleteByObject")
+    @Test
+    void deleteBDD() {
+        // when
+        service.delete(visit);
+
+        // then
+        then(repo).should().delete(any(Visit.class));
+    }
+
     @DisplayName("Test DeleteByID")
     @Test
     void deleteById() {
         service.deleteById(1L);
         verify(repo).deleteById(1L);
+    }
+
+    @DisplayName("BDD Test DeleteByID")
+    @Test
+    void deleteByIdBDD() {
+        // when
+        service.deleteById(1L);
+
+        // then
+         then(repo). should().deleteById(anyLong());
     }
 }
