a3aefb4b5ba9691c8e7ccf49b078f606c7925589
jalnor <norrisj0612@gmail.com>
2021-08-29 21:36:43 -0400

    Added some matches and displaynames to tests.

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
@@ -3,19 +3,19 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.repositories.VisitRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -35,34 +35,42 @@ class VisitSDJpaServiceTest {
         visit = new Visit();
     }
 
+    @DisplayName("Test FindAll")
     @Test
     void findAll() {
         when(repo.findAll()).thenReturn(Set.of(new Visit(), new Visit(), new Visit()));
         Set<Visit> foundVisits = service.findAll();
         assertThat(foundVisits).isNotNull();
+        assertThat(foundVisits).hasSize(3);
         verify(repo).findAll();
     }
 
+    @DisplayName("Test FindByID")
     @Test
     void findById() {
         when(repo.findById(1L)).thenReturn(Optional.of(visit));
         Visit foundVisit = service.findById(1L);
         assertThat(foundVisit).isNotNull();
-        verify(repo).findById(1L);
+        verify(repo).findById(anyLong());
     }
 
+    @DisplayName("Test SaveByObject")
     @Test
     void save() {
-        service.save(visit);
-        verify(repo).save(visit);
+        when(repo.save(any(Visit.class))).thenReturn(visit);
+        Visit savedVisit = service.save(visit);
+        verify(repo).save(any(Visit.class));
+        assertThat(savedVisit).isEqualTo(visit);
     }
 
+    @DisplayName("Test DeleteByObject")
     @Test
     void delete() {
         service.delete(visit);
         verify(repo).delete(visit);
     }
 
+    @DisplayName("Test DeleteByID")
     @Test
     void deleteById() {
         service.deleteById(1L);
